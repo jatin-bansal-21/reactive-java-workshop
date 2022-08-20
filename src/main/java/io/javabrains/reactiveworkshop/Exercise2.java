237bf9c0968eb048ecd05d5be038d8be7dab1b70
Jatin Bansal <88074939+jatin-bansal-21@users.noreply.github.com>
2022-08-20 14:16:30 +0530

    solution: Exercise2

--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -2,6 +2,9 @@ package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
 
+import static io.javabrains.reactiveworkshop.ReactiveSources.intNumbersFlux;
+import static io.javabrains.reactiveworkshop.ReactiveSources.userFlux;
+
 public class Exercise2 {
 
     public static void main(String[] args) throws IOException {
@@ -9,13 +12,17 @@ public class Exercise2 {
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
-        // TODO: Write code here
+        intNumbersFlux().subscribe(Exercise2::print);
 
         // Print all users in the ReactiveSources.userFlux stream
-        // TODO: Write code here
+        userFlux().map(User::getFirstName).subscribe(Exercise2::print);
 
         System.out.println("Press a key to end");
         System.in.read();
+    }
+
+    private static void print(Object obj) {
+        System.out.println(obj);
     }
 
 }
