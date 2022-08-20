9942e1be9553b98d34bbfdd87f394f47187e9be1
Jatin Bansal <88074939+jatin-bansal-21@users.noreply.github.com>
2022-08-20 13:03:12 +0530

    solution: Exercise1

--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -12,32 +12,26 @@ public class Exercise1 {
         // Use StreamSources.intNumbersStream()() and StreamSources.userStream()()
 
         // Print all numbers in the intNumbersStream() stream
-        // TODO: Write code here
         intNumbersStream().forEach(Exercise1::print);
         System.out.println();
 
         // Print numbers from intNumbersStream() that are less than 5
-        // TODO: Write code here
         intNumbersStream().filter(integer -> integer < 5).forEach(Exercise1::print);
         System.out.println();
 
         // Print the second and third numbers in intNumbersStream() that's greater than 5
-        // TODO: Write code here
         intNumbersStream().filter(integer -> integer > 5).skip(1).limit(2).forEach(Exercise1::print);
         System.out.println();
 
         //  Print the first number in intNumbersStream() that's greater than 5.
         //  If nothing is found, print -1
-        // TODO: Write code here
-        intNumbersStream().filter(integer -> integer > 5).findAny().ifPresentOrElse(Exercise1::print, () -> Exercise1.print(-1));
+        intNumbersStream().filter(integer -> integer > 5).findFirst().ifPresentOrElse(Exercise1::print, () -> Exercise1.print(-1));
         System.out.println();
 
         // Print first names of all users in userStream()
-        // TODO: Write code here
         userStream().map(User::getFirstName).forEach(Exercise1::print);
         System.out.println();
         // Print first names in userStream() for users that have IDs from number stream
-        // TODO: Write code here
         Set<Integer> userIds = intNumbersStream().collect(Collectors.toSet());
         userStream().filter(user -> userIds.contains(user.getId())).map(User::getFirstName).forEach(Exercise1::print);
 
